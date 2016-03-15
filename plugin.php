0c5186fef4159a06fa8ed5ab6a6678d294fb3e51
jcopado <>
2016-03-15 17:48:58 +0100

    test2

--- plugin.php
+++ plugin.php
@@ -17,5 +17,5 @@ $ilias_max_version = "5.1.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Fred Neumann, Jesus Copado";
-$responsible_mail = "fred.neumann@ili.fau.de, jesus.copado@ili.fau.de";
+$responsible_mail = "fred.neumann@ili.fau.de, jesus.copado@ili.fau.de 33";
 ?>
