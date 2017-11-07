ba6dd757fe75ef774a0d11338eb71735319413fa
jcopado <jesus.copado@ili.fau.de>
2017-11-07 16:48:34 +0100

    Solve bug #21536 regarding deployed seeds on a test.

--- classes/class.assStackQuestion.php
+++ classes/class.assStackQuestion.php
@@ -1774,7 +1774,8 @@ class assStackQuestion extends assQuestion implements iQuestionCondition
 		{
 			foreach ($this->getPotentialResponsesTrees() as $prt)
 			{
-				$query = $ilDB->query("SELECT tst_solutions.value2 FROM tst_solutions WHERE active_fi = " . $ilDB->quote($active_id, 'integer') . " AND pass = " . $ilDB->quote($pass, 'integer') . " AND value1 = 'xqcas_prt_" . $prt->getPRTName() . "_seed'");
+				//Solve https://www.ilias.de/mantis/view.php?id=21536 bug
+				$query = $ilDB->query("SELECT tst_solutions.value2 FROM tst_solutions WHERE active_fi = " . $db->quote($active_id, 'integer') . " AND pass = " . $db->quote($pass, 'integer') . " AND value1 = 'xqcas_prt_" . $prt->getPRTName() . "_seed'". " AND question_fi = ". $this->getId());
 				$data = $ilDB->fetchAssoc($query);
 				if ($data["value2"])
 				{
