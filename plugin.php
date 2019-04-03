c962c450f67045211a47c244f1ae769aaef98770
jcopado <jesus.copado@ili.fau.de>
2019-04-03 15:23:47 +0200

    3.0.20

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $id = "xqcas";
  
 // code version; must be changed for all code changes
 
-$version = "3.0.17";
+$version = "3.0.20";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
