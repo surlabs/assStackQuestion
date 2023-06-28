19a21cb607a6ba96b4d0b87406dfec9b0bf29d82
jcopado <jcopado@surlabs.es>
2023-06-28 19:56:38 +0200

    htmlspecialchars

--- classes/utils/class.assStackQuestionInitialization.php
+++ classes/utils/class.assStackQuestionInitialization.php
@@ -958,7 +958,9 @@ if (!function_exists('s')) {
         // When we move to PHP 5.4 as a minimum version, change ENT_QUOTES on the
         // next line to ENT_QUOTES | ENT_HTML5 | ENT_SUBSTITUTE, and remove the
         // 'UTF-8' argument. Both bring a speed-increase.
-        return preg_replace('/&amp;#(\d+|x[0-9a-f]+);/i', '&#$1;', htmlspecialchars($var, ENT_QUOTES, 'UTF-8'));
+        if(is_string($var)){
+            return preg_replace('/&amp;#(\d+|x[0-9a-f]+);/i', '&#$1;', htmlspecialchars($var, ENT_QUOTES, 'UTF-8'));
+        }
     }
 }
 
