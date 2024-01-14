b5ab62ec6a3932f8470a346c99f7b72fdd4b6d58
jcopado <jcopado@surlabs.es>
2024-01-14 03:09:20 +0100

    version 7.4.9

--- plugin.php
+++ plugin.php
@@ -1,12 +1,26 @@
 <?php
 /**
- * Copyright (c) Laboratorio de Soluciones del Sur, Sociedad Limitada
- * GPLv3, see LICENSE
+ *  This file is part of the STACK Question plugin for ILIAS, an advanced STEM assessment tool.
+ *  This plugin is developed and maintained by SURLABS and is a port of STACK Question for Moodle,
+ *  originally created by Chris Sangwin.
+ *
+ *  The STACK Question plugin for ILIAS is open-source and licensed under GPL-3.0.
+ *  For license details, visit https://www.gnu.org/licenses/gpl-3.0.en.html.
+ *
+ *  To report bugs or participate in discussions, visit the Mantis system and filter by
+ *  the category "STACK Question" at https://mantis.ilias.de.
+ *
+ *  More information and source code are available at:
+ *  https://github.com/surlabs/STACK
+ *
+ *  If you need support, please contact the maintainer of this software at:
+ *  stack@surlabs.es
+ *
  */
 
 $id = "xqcas";
  
-$version = "7.4.8";
+$version = "7.4.9";
 
 $ilias_min_version = "7.13";
 $ilias_max_version = "7.999";
