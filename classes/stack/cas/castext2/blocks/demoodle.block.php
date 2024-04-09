5c8bc87f7fbfd685879e823e03bdb5b1163f2153
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-04-09 17:51:01 +0200

    Update demoodle.block.php

--- classes/stack/cas/castext2/blocks/demoodle.block.php
+++ classes/stack/cas/castext2/blocks/demoodle.block.php
@@ -70,7 +70,7 @@ class stack_cas_castext2_demoodle extends stack_cas_castext2_block {
                 $r .= $v;
             } else {
                 // Parameters as they would be if this were called through the question->format_text.
-                $r .= text_to_html($v, null, false, true);
+                $r .= $v;
             }
         }
 
