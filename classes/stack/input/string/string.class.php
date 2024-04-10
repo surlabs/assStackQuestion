b8800e9b72f57548e79fb4312598d92c6b7ca1e9
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-04-10 18:43:21 +0200

    Correct input width

--- classes/stack/input/string/string.class.php
+++ classes/stack/input/string/string.class.php
@@ -38,7 +38,7 @@ class stack_string_input extends stack_algebraic_input {
             return $this->render_error($this->errors);
         }
 
-        $size = $this->parameters['boxWidth'] * 0.9 + 0.1;
+        $size = $this->parameters['boxWidth'] + 0.2;
 
         if ($readonly){
             $solution_input_id = $fieldname . '_sol';
@@ -48,7 +48,7 @@ class stack_string_input extends stack_algebraic_input {
             'type'  => 'text',
             'name'  => $fieldname,
             'id'    => $fieldname,
-            'size'  => $this->parameters['boxWidth'] * 1.1,
+            'size'  => $size,
             'style' => 'width: '.$size.'em',
             'autocapitalize' => 'none',
             'spellcheck'     => 'false',
--- classes/stack/input/units/units.class.php
+++ classes/stack/input/units/units.class.php
@@ -51,7 +51,7 @@ class stack_units_input extends stack_input {
             return $this->render_error($this->errors);
         }
 
-        $size = $this->parameters['boxWidth'] * 0.9 + 0.1;
+        $size = $this->parameters['boxWidth'] + 0.2;
         if ($readonly){
             $solution_input_id = $fieldname . '_sol';
             $fieldname = $solution_input_id;
@@ -60,7 +60,7 @@ class stack_units_input extends stack_input {
             'type'  => 'text',
             'name'  => $fieldname,
             'id'    => $fieldname,
-            'size'  => $this->parameters['boxWidth'] * 1.1,
+            'size'  => $size,
             'style' => 'width: '.$size.'em',
             'autocapitalize' => 'none',
             'spellcheck'     => 'false',
