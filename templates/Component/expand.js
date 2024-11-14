c51ed20d9641039bc7b77a95f2712106b10a1ec6
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-11-14 12:47:02 +0100

    Display the panel from the panel header

--- templates/Component/expand.js
+++ templates/Component/expand.js
@@ -1,34 +1,40 @@
-$(document).ready(function() {
-  $(".viewcontrol-expand").click(function() {
-    let new_state = $(this).attr("data-state") === "collapsed";
+$(".panel .panel-heading.ilHeader").click(function() {
+  $child = $(this).find(".viewcontrol-expand");
 
-    $(this).find("img").attr("src", `/Customizing/global/plugins/Modules/TestQuestionPool/Questions/assStackQuestion/templates/Component/tree_${new_state ? "exp" : "col"}.svg`);
-    $(this).find("img").attr("alt", new_state ? "Expanded" : "Collaped");
-    $(this).attr("data-state", new_state ? "expanded" : "collapsed");
-
-    const $content = $(this).parent().parent().find(".panel-body");
-
-    if (new_state) {
-      $content.slideDown();
-    } else {
-      $content.slideUp();
-    }
-  });
-
-  $(".viewcontrol-expand").each(function() {
-    // Poner el boton como primer elemento del div
-    $(this).parent().prepend($(this));
-
-    $(this).parent().css("justify-content", "normal").css("align-items", "center");
-
-    $(this).css("margin-right", "10px");
-
-    if ($(this).attr("data-state") === "collapsed") {
-      $(this).parent().parent().find(".panel-body").hide();
-    }
-
-    if ($(this).parent().find(".panel-viewcontrols").empty()) {
-      $(this).parent().find(".panel-viewcontrols").remove();
-    }
-  });
+  if ($child.length > 0) {
+    triggerExpand($child);
+  }
 });
+
+$(".viewcontrol-expand").each(function() {
+  // Poner el boton como primer elemento del div
+  $(this).parent().prepend($(this));
+
+  $(this).parent().css("justify-content", "normal").css("align-items", "center");
+
+  $(this).css("margin-right", "10px");
+
+  if ($(this).attr("data-state") === "collapsed") {
+    $(this).parent().parent().find(".panel-body").hide();
+  }
+
+  if ($(this).parent().find(".panel-viewcontrols").empty()) {
+    $(this).parent().find(".panel-viewcontrols").remove();
+  }
+});
+
+function triggerExpand($this) {
+  let new_state = $($this).attr("data-state") === "collapsed";
+
+  $($this).find("img").attr("src", `/Customizing/global/plugins/Modules/TestQuestionPool/Questions/assStackQuestion/templates/Component/tree_${new_state ? "exp" : "col"}.svg`);
+  $($this).find("img").attr("alt", new_state ? "Expanded" : "Collaped");
+  $($this).attr("data-state", new_state ? "expanded" : "collapsed");
+
+  const $content = $($this).parent().parent().find(".panel-body");
+
+  if (new_state) {
+    $content.slideDown();
+  } else {
+    $content.slideUp();
+  }
+}
